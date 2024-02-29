7638f45131856d2be54c8993fb2ce266f8ca3003
jonhavbra87 <jon.are.br@gmail.com>
2024-02-29 16:31:59 +0100

    v1.6
    Fixed footer and form button

--- js/ui/backToTop.js
+++ js/ui/backToTop.js
@@ -1,18 +1,43 @@
 export function backToTopButton() {
-    document.addEventListener('DOMContentLoaded', function () {
-    const backToTopButton = document.getElementById('backToTop');
+  document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
 
     window.onscroll = function () {
+      // Sjekker om skjermen er større enn 1024 piksler i bredde
+      if (window.innerWidth > 800) {
         if (window.pageYOffset > 300) {
-            backToTopButton.style.display = 'block';
-            backToTopButton.classList.add('back-to-top');
+          backToTopButton.style.display = "block";
+          backToTopButton.classList.add("back-to-top");
         } else {
-            backToTopButton.style.display = 'none';
+          backToTopButton.style.display = "none";
         }
+      } else {
+        // Skjuler knappen på mindre skjermer uavhengig av scroll-posisjon
+        backToTopButton.style.display = "none";
+      }
     };
 
     backToTopButton.onclick = function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     };
-});
+  });
 }
+
+// export function backToTopButton() {
+//     document.addEventListener('DOMContentLoaded', function () {
+//     const backToTopButton = document.getElementById('backToTop');
+
+//     window.onscroll = function () {
+//         if (window.pageYOffset > 300) {
+//             backToTopButton.style.display = 'block';
+//             backToTopButton.classList.add('back-to-top');
+//         } else {
+//             backToTopButton.style.display = 'none';
+//         }
+//     };
+
+//     backToTopButton.onclick = function () {
+//         window.scrollTo({ top: 0, behavior: 'smooth' });
+//     };
+// });
+// }
